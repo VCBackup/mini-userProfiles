9b9b33f6118cb4316f6b1225150c3733123a26f3
Moore <DMoore@isolvedhcm.com>
2017-05-24 19:37:19 -0600

    Initial commit with some work completed.

--- controller.js
+++ controller.js
@@ -1,5 +1,10 @@
-angular.module('userProfiles').controller('MainController', function($scope){
+angular.module('userProfiles').controller('MainController', 
+function($scope, mainService){
     
+    $scope.getUsers = function(){
+        $scope.users = mainService.getUsers();
+        return $scope.users;
+    };
     $scope.thisAppIsBroken = "This angular app is working";
    
 })
